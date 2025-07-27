b485a09b06345c940eb5b9f3f4d26f0c844e1afa
George Tukov <georgetukov21@gmail.com>
2025-07-27 19:08:00 +0300

    MP-9: Добавлена структура проекта с HelloWorld и HelloGit

---
+++ src/main/java/ru/mentee/power/HelloGit.java/src/Main.java
@@ -0,0 +1,7 @@
+package ru.mentee.power;
+public class Main {
+    public static void main(String [] args) {
+        System.out.println("Привет, Git и Github!");
+        System.out.println("Я начинаю изучение Java!");
+    }
+}
